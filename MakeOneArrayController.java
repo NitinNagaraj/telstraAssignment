20321ff29cd9468b779290d04138cb334eb97e89
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-05 19:07:35 +0530

    commit files

---
+++ MakeOneArrayController.java
@@ -0,0 +1,12 @@
+/*package com.hcl.telstraAssignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api",method=RequestMethod.POST)
+public class MakeOneArrayController {
+
+}
+*/
